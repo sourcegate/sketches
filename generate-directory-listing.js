6f879b62f86e179cea08eb8bcc1f49e040ba7cc2
sourcegate <pete@digitalsurgeons.com>
2023-08-08 18:48:48 -0400

    x

--- generate-directory-listing.js
+++ generate-directory-listing.js
@@ -36,7 +36,7 @@ const htmlTemplate = `
    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    // <link href="/dist/output.css" rel="stylesheet">
+     <link href="/dist/output.css" rel="stylesheet">
     <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="/dist/output.css" rel="stylesheet">
